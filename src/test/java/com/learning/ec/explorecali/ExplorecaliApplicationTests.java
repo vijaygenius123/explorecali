832bc2ff96a20ed533258e85e62d10de03329074
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-12-03 20:45:58 +0530

    Initial commit

---
+++ src/test/java/com/learning/ec/explorecali/ExplorecaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ec.explorecali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorecaliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
